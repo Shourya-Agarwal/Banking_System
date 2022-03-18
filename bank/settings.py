589d84cab997a09458f9eb8aa8cf8cbcb00a750a
Shourya Agarwal <Shourya17agarwal@gmail.com>
2022-03-18 20:10:43 +0530

    second commit

--- bank/settings.py
+++ bank/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@=rk$0=o0$jyq_t&=@m69zz9@dg26b&hn4dkgk-9jdat(z-1c+'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
